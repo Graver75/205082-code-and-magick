e2563a9ce846a17128ee89925a4f0236a3dde176
Graver <undergraver75@gmail.com>
2016-08-07 21:05:16 +0300

    Внесены правки

--- build/js/check.js
+++ build/js/check.js
@@ -1,46 +1,44 @@
 function getMessage (a, b) {
-	if (typeof(a) === 'boolean') {
-		if (a === true) {
+	if (typeof a === 'boolean') {
+		if (a) {
 			var sMessage = 'Я попал в ' + b;
 			return sMessage;
 		}
-		if (a === false) {
+		else {
 			var sMessage = 'Я никуда не попал';
 			return sMessage;
 		}
 	}
- 	if (typeof(a) === 'number') {
-		var sMessage = 'Я прыгнул на ' + a*100 + ' сантиметров';
+ 	if (typeof a === 'number') {
+		var sMessage = 'Я прыгнул на ' + a * 100 + ' сантиметров';
 		return sMessage;
 	}
-	if (typeof(a) === 'object') {
+	if (typeof a === 'object') {
 		var sMessage = 'Я прошёл ' + setSumm(a) + ' шагов';
 		return sMessage;
 	}
-	if(typeof(b) === 'object' && typeof(a) == 'object') {
+	if(typeof b === 'object' && typeof(a) == 'object') {
 		var sMessage = 'Я прошёл ' + setPath(a, b) + ' метров';
 		return sMessage;
 	}
 }
 
+var summ = 0;
+var i;
 function setPath (a, b) {
-	var summ = 0;
-	var i;
-	for(i=0; i<=a.length; i++) {
+	for(i = 0; i <= a[a.length - 1]; i++) {
 		summ = summ + a[i] * b[i];
 
-		if (i === (a.length-1)) {
+		if (i === (a.length - 1)) {
 			return summ;
 		}
 
 	}
 }
 function setSumm (a) {
-	var summ = 0;
-	var i;
-	for(i=0; i<=a.length; i++) {
+	for(i =0; i <= a[a.length - 1]; i++) {
 		summ = summ + a[i];
-		if (i === (a.length-1)) {
+		if (i === (a.length - 1)) {
 			return summ;
 		}
 	}
