0b86cfcbaba84f2d0bad3c6fd2e28207e2229166
Graver <undergraver75@gmail.com>
2016-08-07 21:59:46 +0300

    Сократил код

--- build/js/check.js
+++ build/js/check.js
@@ -3,24 +3,20 @@ function getMessage (a, b) {
 	if (typeof a === 'boolean') {
 		if (a) {
 			sMessage = 'Я попал в ' + b;
-			return sMessage;
 		} else {
 			sMessage = 'Я никуда не попал';
-			return sMessage;
 		}
 	}
  	if (typeof a === 'number') {
 		sMessage = 'Я прыгнул на ' + a * 100 + ' сантиметров';
-		return sMessage;
 	}
 	if (typeof a === 'object') {
 		sMessage = 'Я прошёл ' + setSumm(a) + ' шагов';
-		return sMessage;
 	}
 	if (typeof b === 'object' && typeof a == 'object') {
 		sMessage = 'Я прошёл ' + setPath(a, b) + ' метров';
-		return sMessage;
 	}
+	return sMessage;
 }
 
 function setPath (a, b) {	
