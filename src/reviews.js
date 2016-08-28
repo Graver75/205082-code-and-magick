795176d391a713d1dc2b29431ad0d9d925047d13
Graver75 <undergraver75@gmail.com>
2016-08-28 22:29:27 +0300

    Коммит в котором ничего не работает

--- src/reviews.js
+++ src/reviews.js
@@ -3,17 +3,19 @@
 var REVIEWS_LOAD_URL = 'http://localhost:1506/api/reviews';
 var reviews;
 
-var load = function(url, callback) {
-  window[callback] = function(data) {
-    console.log(data);
+var load = function(url, callback, callbackName) {
+  window[callbackName] = function(data) {
+    callback(data);
   };
 
   var script = document.createElement('script');
-  script.src = url + '?callback=' + callback;
+  script.src = url + '?callback=' + callbackName;
   document.body.appendChild(script);
 };
 
-load(REVIEWS_LOAD_URL, '__jsonpCallback');
+load(REVIEWS_LOAD_URL,function(data) {
+  reviews = data;
+},'__jsonpCallback');
 console.log(reviews);
 
 
