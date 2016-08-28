7b19bcf64820cee4ccde3a5f52ec4828f1d683bc
Graver75 <undergraver75@gmail.com>
2016-08-28 12:26:46 +0300

    Какая та проблема с подключениями

--- src/review.js
+++ src/review.js
@@ -8,13 +8,13 @@ var load = function(url, callback, callbackName) {
   }
   window[callbackName] = function(data) {
     callback(data);
-  }
+  };
 
   var script = document.createElement('script');
   script.src = url + '?callback=' + callbackName;
   document.body.appendChild(script);
 };
 
-load(REVIEWS_LOAD_URL, function(data)) {
+load(REVIEWS_LOAD_URL, function(data) {
   console.log(data);
 }, '__jsonpCallback');
