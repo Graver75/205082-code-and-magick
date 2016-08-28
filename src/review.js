37b0742eeb2929ff32a434145b0a6b73179490db
Graver75 <undergraver75@gmail.com>
2016-08-28 12:06:14 +0300

    Эксперементальная функция обработки jsonp

---
+++ src/review.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var REVIEWS_LOAD_URL = 'http://localhost:1506/api/reviews';
+
+var load = function(url, callback, callbackName) {
+  if (!callbackName) {
+    callbackName = 'cb' + Date.now();
+  }
+  window[callbackName] = function(data) {
+    callback(data);
+  }
+
+  var script = document.createElement('script');
+  script.src = url + '?callback=' + callbackName;
+  document.body.appendChild(script);
+};
+
+load(REVIEWS_LOAD_URL, function(data)) {
+  console.log(data);
+}, '__jsonpCallback');
