80c7175eedf34872af22b6b85d23eaae1fcdd4f2
Graver75 <undergraver75@gmail.com>
2016-08-29 13:18:42 +0300

    Да вроде я молодец. Повесил обработчики, даже смог звездочки настроить. Уу

--- src/reviews.js
+++ src/reviews.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var templateElement = document.querySelector('#review-template');
+var reviewsList = document.querySelector('.reviews-list');
 var REVIEWS_LOAD_URL = 'http://localhost:1506/api/reviews';
 var reviews;
+var filters = document.querySelector('reviews-filter');
 
 var load = function(url, callback, callbackName) {
   window[callbackName] = function(data) {
@@ -16,7 +19,35 @@ var load = function(url, callback, callbackName) {
 load(REVIEWS_LOAD_URL, function(data) {
   reviews = data;
   console.log(reviews);
+  reviews.forEach(function (element) {
+    showReviews.placing(element);
+  });
 }, '__jsonpCallback');
 
+var showReviews  = {
+  getclone: function () {
+    var tmpl = templateElement.cloneNode(true);
+    tmpl = tmpl.content.querySelector('.review');
+    return tmpl;
+  },
+  placing: function(element) {
+    var tmpl = this.getclone();
+    console.dir(tmpl);
+    tmpl.querySelector('.review-text').textContent = element.description;
+    tmpl.querySelector('.review-rating').style.width = 40 * element.rating + 'px';
+    tmpl.querySelector('.review-rating').style.maxWidth = '200px';
 
+    var img = new Image(124, 124);
+    img.onload = function () {
+      tmpl.querySelector('.review-author').src = element.author.picture;
+    };
+    img.onerror = function () {
+      console.log('Error loading img ' + img.src);
+      tmpl.querySelector('.review-author').classList.add('review-load-failure');
+    }
+    img.src = element.author.picture;
+    console.dir(img);
 
+    reviewsList.appendChild(tmpl);
+  }
+};
