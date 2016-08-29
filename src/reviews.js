07667bb8a9739eca2e498295e6cc5a798a7e2826
Graver75 <undergraver75@gmail.com>
2016-08-29 13:30:17 +0300

    Скрыл фильтры, мелкие правки

--- src/reviews.js
+++ src/reviews.js
@@ -4,7 +4,7 @@ var templateElement = document.querySelector('#review-template');
 var reviewsList = document.querySelector('.reviews-list');
 var REVIEWS_LOAD_URL = 'http://localhost:1506/api/reviews';
 var reviews;
-var filters = document.querySelector('reviews-filter');
+var filters = document.querySelector('.reviews-filter');
 
 var load = function(url, callback, callbackName) {
   window[callbackName] = function(data) {
@@ -19,18 +19,19 @@ var load = function(url, callback, callbackName) {
 load(REVIEWS_LOAD_URL, function(data) {
   reviews = data;
   console.log(reviews);
-  reviews.forEach(function (element) {
+  reviews.forEach(function(element) {
     showReviews.placing(element);
   });
 }, '__jsonpCallback');
 
-var showReviews  = {
-  getclone: function () {
+var showReviews = {
+  getclone: function() {
     var tmpl = templateElement.cloneNode(true);
     tmpl = tmpl.content.querySelector('.review');
     return tmpl;
   },
   placing: function(element) {
+    filters.classList.add('invisible');
     var tmpl = this.getclone();
     console.dir(tmpl);
     tmpl.querySelector('.review-text').textContent = element.description;
@@ -38,16 +39,17 @@ var showReviews  = {
     tmpl.querySelector('.review-rating').style.maxWidth = '200px';
 
     var img = new Image(124, 124);
-    img.onload = function () {
+    img.onload = function() {
       tmpl.querySelector('.review-author').src = element.author.picture;
     };
-    img.onerror = function () {
+    img.onerror = function() {
       console.log('Error loading img ' + img.src);
       tmpl.querySelector('.review-author').classList.add('review-load-failure');
-    }
+    };
     img.src = element.author.picture;
     console.dir(img);
 
     reviewsList.appendChild(tmpl);
   }
 };
+filters.classList.remove('invisible');
