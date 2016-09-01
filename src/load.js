db9a99127fe6bce45cd83e22ec783f0d14ad79b1
Graver75 <undergraver75@gmail.com>
2016-09-01 13:41:46 +0300

    Обернул в define

--- src/load.js
+++ src/load.js
@@ -1,20 +1,17 @@
 'use strict';
 
 var arrReviews;
-
-var load = function(url, callback, callbackName) {
-  window[callbackName] = function(data) {
-    callback(data);
-  };
-
-  var script = document.createElement('script');
-  script.src = url + '?callback=' + callbackName;
-  document.body.appendChild(script);
-  window[callbackName](arrReviews);
-};
-
 define(function () {
-  return load;
+  return function (url, callback, callbackName) {
+    window[callbackName] = function (data) {
+      callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + '?callback=' + callbackName;
+    document.body.appendChild(script);
+    window[callbackName](arrReviews);
+  };
 });
 
 
