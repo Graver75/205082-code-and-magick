6ab1270d847df78a9ad37bd6d3e480b4b1a27b6c
Graver75 <undergraver75@gmail.com>
2016-09-03 22:27:36 +0300

    Подключил модуль галереи

--- src/gallery.js
+++ src/gallery.js
@@ -1,16 +1,54 @@
 'use strict';
-Gallery.prototype.show = function(number) {
-  this.galleryElement.classList.remove('invisible');
-  this.addListeners();
-  this.setActivePicture(number);
-};
-Gallery.prototype.hide = function() {
-  this.galleryElement.classList.add('invisible');
-
-};
 
 define(function() {
-  return function(pictures) {
+  var Gallery = function(pictures) {
+    Gallery.prototype.show = function(number) {
+      galleryElement.classList.remove('invisible');
+      this.addListeners();
+      this.setActivePicture(number);
+    };
+    Gallery.prototype.hide = function() {
+      galleryElement.classList.add('invisible');
+    };
+    Gallery.prototype.setActivePicture = function(number) {
+      this.activePicture = number;
+      currentNumberElement.innerText = number;
+
+      var img = new Image();
+
+      img.onerror = function() {
+        console.log("Error loading image in gallery");
+      };
+
+      img.src = this.pictures[number - 1];
+
+      if (previewGalleryElement.querySelector('img')) {
+        previewGalleryElement.replaceChild(img, previewGalleryElement.querySelector('img'));
+      }
+      previewGalleryElement.appendChild(img);
+    };
+    Gallery.prototype.addListeners = function() {
+      controlRightElement.onclick = function () {
+        if (self.activePicture < TOTAL_PICTURES) {
+          self.activePicture++;
+          self.setActivePicture(self.activePicture);
+        }
+      };
+      controlLeftElement.onclick = function () {
+        if (self.activePicture !== 1) {
+          self.activePicture--;
+          self.setActivePicture(self.activePicture);
+        }
+      };
+      closeButtonElement.onclick = function () {
+        self.hide();
+      };
+    };
+    Gallery.prototype.removeListeners = function() {
+      self.controlRightElement.onclick = null;
+      self.controlLeftElement.onclick = null;
+      self.closeButtonElement.onclick = null;
+    };
     var galleryElement = document.querySelector('.overlay-gallery');
     var controlLeftElement = document.querySelector('.overlay-gallery-control-left');
     var controlRightElement = document.querySelector('.overlay-gallery-control-right');
@@ -20,50 +58,10 @@ define(function() {
     var previewGalleryElement = document.querySelector('.overlay-gallery-preview');
     var self = this;
     var TOTAL_PICTURES = pictures.length;
+    totalNumberElement.innerText = TOTAL_PICTURES;
 
     this.pictures = pictures;
     this.activePicture = 1;
-
-    this.setActivePicture = function(number) {
-      this.activePicture = number;
-      currentNumberElement.innerText = number;
-
-      var img = new Image();
-
-      img.onerror = function() {
-        console.log("Error loading image in gallery");
-      }
-
-      img.src = this.pictures[number - 1];
-
-      if (previewGalleryElement.document.querySelector('img')) {
-        previewGalleryElement.replaceChild(img, previewGalleryElement.document.querySelector('img'));
-      }
-      previewGalleryElement.appendChild(img);
-    };
-
-    this.addListeners = function() {
-      self.controlRightElement.onclick = function() {
-        if(activePicture < TOTAL_PICTURES) {
-          activePicture++;
-          setActivePicture(activePicture);
-        }
-      };
-
-      self.controlLeftElement.onclick = function() {
-        if(activePicture !== 1) {
-          activePicture--;
-          setActivePicture(activePicture);
-        }
-      };
-      self.closeButtonElement.onclick = function() {
-        hide();
-      };
-    };
-    this.removeListeners = function() {
-      self.controlRightElement.onclick = null;
-      self.controlLeftElement.onclick = null;
-      self.closeButtonElement.onclick = null;
-    };
   };
+  return Gallery;
 });
--- src/main.js
+++ src/main.js
@@ -1,13 +1,49 @@
 'use strict';
 define([
-  './game', './form', './gallery', './reviews'
-], function(gameObj, form, gallery, reviews) {
+  './game',
+  './form',
+  './gallery',
+  './reviews'
+], function(gameObj, form, GalleryObj) {
   var Game = gameObj();
   var game = new Game(document.querySelector('.demo'));
-  var galleryContainer = document.querySelector('.main-section.photogallery');
-  var pictures = galleryContainer['img'];
-  var Gallery = gallery();
-  var gallery = new Gallery()
+  var section = document.querySelector('.photogallery');
+  var pictures = [
+    section.childNodes[3].childNodes[0].src,
+    section.childNodes[5].childNodes[0].src,
+    section.childNodes[7].childNodes[0].src,
+    section.childNodes[9].childNodes[0].src,
+    section.childNodes[11].childNodes[0].src,
+    section.childNodes[13].childNodes[0].src
+  ];
+  var gallery = new GalleryObj(pictures);
+  var links = [
+    section.childNodes[3],
+    section.childNodes[5],
+    section.childNodes[7],
+    section.childNodes[9],
+    section.childNodes[11],
+    section.childNodes[13]
+  ];
+  links[0].onclick = function() {
+    gallery.show(1);
+  };
+  links[1].onclick = function() {
+    gallery.show(2);
+  };
+  links[2].onclick = function() {
+    gallery.show(3);
+  };
+  links[3].onclick = function() {
+    gallery.show(4);
+  };
+  links[4].onclick = function() {
+    gallery.show(5);
+  };
+  links[5].onclick = function() {
+    gallery.show(6);
+  };
+
 
   game.initializeLevelAndStart();
   game.setGameStatus(Game.Verdict.INTRO);
